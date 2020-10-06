3d999886e64c4953fcc6050f32bd3d62f11e78c0
César Guerra <warderer@gmail.com>
2020-10-05 21:49:09 -0500

    webpack config to support HTML

---
+++ clases/15.Webpack/webpack-starter/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module:{
+        rules:[ //Aquí se cargan los Loaders de WebPack
+            {
+                test: /\.html$/, //REGEX -> Busca todos los archivos que terminan en .html
+                use: [
+                    {
+                        loader: "html-loader", //Traduce HTML para que webpack lo entienda
+                        options: {minimize:true} //Minifica los archivos HTML encontrados
+                    }
+                ]
+                //test: Significa que tengo que buscar
+                //use: de lo que encontre, que Loader voy a aplicar
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:"./src/index.html", //Que archivo HTML va a ser el base de mi proyecto en la carpeta src
+            filename:"./index.html" // Que único archivo de HTML se va a generar en la carpeta dist
+            //El archivo de conf. de webpack simula que se trabaja desde la carpeta dist, por lo que no se necesita especificar.
+        })
+    ]
+}

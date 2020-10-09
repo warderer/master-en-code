9f0c149204f75dd3cd6d331a184d0a038e2940f1
César Guerra <warderer@gmail.com>
2020-10-08 21:39:05 -0500

    webpack-pokeapi/get data from pokeapi with axios

--- clases/15.Webpack/webpack-pokedex/src/index.js
+++ clases/15.Webpack/webpack-pokedex/src/index.js
@@ -1,4 +1,17 @@
-//<!-- Bootstrap -->
+import axios from 'axios';
 import 'bootstrap';
-
 import './styles/main.scss';
+
+function init(){ //App Start
+    //Solicitar los datos de los Pokemons
+    const URI = 'https://pokeapi.co/api/v2/pokemon?limit=1050&offset=0';
+    axios.get(URI)
+        .then((response) => {
+            console.log("Status Code:",response.status); //status de la petición
+            console.log(response.data.results); //data es donde tiene la información
+        }).catch((error) => {
+            console.log("Error:",error);
+        });
+}
+
+init();

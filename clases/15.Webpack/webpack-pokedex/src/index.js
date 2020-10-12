d7639122999f177e8b3d234396612cc451e65e24
César Guerra <warderer@gmail.com>
2020-10-12 00:43:05 -0500

    webpack-pokedex/Index limit pokemons

--- clases/15.Webpack/webpack-pokedex/src/index.js
+++ clases/15.Webpack/webpack-pokedex/src/index.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import 'bootstrap';
 import './styles/main.scss';
+import Pokedex from './Pokedex';
 
 function init(){ //App Start
     //Solicitar los datos de los Pokemons
-    const URI = 'https://pokeapi.co/api/v2/pokemon?limit=1050&offset=0';
+    const pokemonCount = 10;
+    const URI = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}&offset=0'`;
     axios.get(URI)
         .then((response) => {
             console.log("Status Code:",response.status); //status de la petición
-            console.log(response.data.results); //data es donde tiene la información
+            //console.log(response.data.results); //data es donde tiene la información
+            const pokedex = new Pokedex(response.data.results);
+            pokedex.createPokedex();
         }).catch((error) => {
             console.log("Error:",error);
         });

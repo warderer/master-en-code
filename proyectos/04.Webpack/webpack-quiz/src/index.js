e1399538346b191f9e4f87e5b483aae7ceb00ce8
César Guerra <warderer@gmail.com>
2020-10-18 16:54:13 -0500

    webpack-quiz/Quiz selection view implementation

--- proyectos/04.Webpack/webpack-quiz/src/index.js
+++ proyectos/04.Webpack/webpack-quiz/src/index.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 import bootstrap from 'bootstrap';
 import Game from './Game';
-//import Quiz from './Quiz';
 import './styles/main.scss';
 
+document.getElementById("startButton").addEventListener("click", function(){
+    event.preventDefault();
+    start();
+});
+
 function start(){
-    //preguntas, categoria, dificultad(easy,medium,hard), tipo(multiple/boolean)
-    const game = new Game(10, 11,'hard','multiple');
+    let category = parseInt(document.getElementById("categorySelect").value);
+    //category = isNaN(category) ? category : parseInt(category);
+    let difficulty = document.getElementById("difficultySelect").value;
+    let type = document.getElementById("typeSelect").value;
+    //preguntas, categoria, dificultad(easy,medium,hard), tipo(multiple/boolean
     //const game = new Game(10,9,'hard','boolean'); //Esta categoria da error, no devuelve preguntas.
-    game.init()
+    const game = new Game(10, category,difficulty,type);
+    console.log(category,difficulty,type);
+    game.init();
 }
 
-start();
+

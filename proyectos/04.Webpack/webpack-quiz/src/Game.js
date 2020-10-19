6fb5e68d8efd1ead0f65ccc12881073209652f0b
César Guerra <warderer@gmail.com>
2020-10-18 21:02:25 -0500

    webpack-quiz/question show with pagination

--- proyectos/04.Webpack/webpack-quiz/src/Game.js
+++ proyectos/04.Webpack/webpack-quiz/src/Game.js
@@ -1,15 +1,26 @@
 import Quiz from './Quiz';
+import QuestionCard from './components/QuestionCard';
 
 class Game {
     constructor(quantity, catId, difficultyLevel, answersType) {
         this._singleAnswerScore = 100; //config: puntaje de cada respuesta
         this._score = 0;
         this._currentQuestion = 0;
+        this._currentAnswers = [];
         this._QuizQuestionsNumber = quantity;
         this._QuizCategoryId = catId; //from Form
         this._QuizDifficulty = difficultyLevel; //from Form
         this._QuizAnswersType = answersType; //from Form
         this._Responses = []; //true si correcto, false si incorrecto
+        this._quizContent = null; //Almacena la respuesta de la API
+    }
+
+    set quizContent(APIresponse){
+        this._quizContent = APIresponse;
+    }
+
+    get quizContent(){
+        return this._quizContent;
     }
 
     get currentQuestion() {
@@ -39,30 +50,63 @@ class Game {
         }
     }
 
+    get currentAnswers(){
+        return this._currentAnswers;
+    }
+
+    set currentAnswers(singleQuestion){
+        const incorrectAnswers = singleQuestion.incorrect_answers;
+        const correctAnswer = singleQuestion.correct_answer;
+        let list = [correctAnswer, ...incorrectAnswers];
+        this._currentAnswers = list.sort(() => Math.random() - 0.5);
+    }
+
+    continue(){
+
+        this.currentAnswers = this.quizContent[this._currentQuestion];
+        document.getElementById('mainContent').innerHTML="";
+        let questionElement = document.getElementById('mainContent');
+        let questionCard = new QuestionCard(this.currentQuestion, this.quizContent[this.currentQuestion].question, this.currentAnswers);
+        questionElement.innerHTML=questionCard.generate();
+        this.nextQuestion();
+
+        document.getElementById("nextButton").addEventListener("click", function(){
+            event.preventDefault();
+            this.continue();
+        }.bind(this)); //Uso Bind para poder traer el this del quiz dentro del Event Listener
+    }
+
     init(){
         const quiz = new Quiz(this._QuizQuestionsNumber, this._QuizCategoryId,this._QuizDifficulty,this._QuizAnswersType);
         quiz.init()
             .then(() => {
                 if (quiz._QuestionsArray!== null) {
                     window.$('.alert').hide();
-                    console.log(quiz._QuestionsArray);
-                    console.log('----')
-                    //Test Sigle Question Pick
-                    console.log(quiz._QuestionsArray.results[this._currentQuestion]);
-                    this.nextQuestion();
-                    console.log('----')
-                    console.log(quiz._QuestionsArray.results[this._currentQuestion]);
-                    this.nextQuestion();
-                    console.log('----')
-                    //Set Score Simulation
-                    this.response = true;
-                    this.response = false;
-                    this.response = true;
-                    this.response = true;
-                    console.log(this._Responses);
-                    console.log(this._score);
+                    // console.log(quiz._QuestionsArray);
+                    // console.log('----');
+                    // //Test Sigle Question Pick
+                    // console.log(quiz._QuestionsArray.results[this._currentQuestion]);
+                    // this.currentAnswers = quiz._QuestionsArray.results[this._currentQuestion];
+                    // console.log(this._currentAnswers);
+                    // this.nextQuestion();
+                    // console.log('----');
+                    // console.log(quiz._QuestionsArray.results[this._currentQuestion]);
+                    // this.currentAnswers = quiz._QuestionsArray.results[this._currentQuestion];
+                    // console.log(this._currentAnswers);
+                    // //this.nextQuestion();
+                    // console.log('----');
+                    // //Set Score Simulation
+                    // this.response = true;
+                    // this.response = false;
+                    // this.response = true;
+                    // this.response = true;
+                    // console.log(this._Responses);
+                    // console.log(this._score);
+
+                    this.quizContent = quiz._QuestionsArray.results;
+                    this.continue();
+                    
                 } else {
-                    //window.$ = window.jQuery = require("jquery");
                     window.$('.alert').show();
                     console.log("No existen preguntas en esa categoría, por favor selecciona otra");
                 }
---
+++ proyectos/04.Webpack/webpack-quiz/src/components/QuestionCard.js
@@ -0,0 +1,50 @@
+class QuestionCard{
+    constructor(number,question,answers){
+        this._number = number; //current question
+        this._question = question;
+        this._answers = answers; //array
+        console.log("questionCard",number,question,answers);
+    }
+
+    answersList(){
+        let options = "";
+        this._answers.forEach((element,index) => {
+            const html = `
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="${index}" value="${element}">
+                        <label class="form-check-label" for="${index}">
+                            ${element}
+                        </label>
+                </div>
+            `;
+            options = options + html;
+        });
+        return options;
+    }
+
+    generate(){
+        const answersHTML = this.answersList();
+        const html=`
+        <h1 class="cover-heading">${this._number+1}</h1>
+        <p class="lead">${this._question}</p>
+        <div class="container mt-5">
+            <form role="form" data-toggle="validator">
+                <div class="row">
+                    <div class="column col-md-12">
+                        <div class="input-group mb-5 mr-sm-2">
+                            <fieldset class="form-group text-left" id="questionSelect">
+                                ${answersHTML}
+                            </fieldset>
+                        </div>
+                    </div>
+
+                    <div class="column col-md-12">
+                        <button type="submit" class="btn btn-info btn-lg" id="nextButton">Next</button>
+                    </div>
+                </div>
+        `
+        return html;
+    }
+}
+
+export default QuestionCard;

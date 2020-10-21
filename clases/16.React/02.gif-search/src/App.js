60e693a2506296844c52bd49d4dde1429e3ef833
César Guerra <warderer@gmail.com>
2020-10-20 20:05:41 -0500

    gif-search/react life cicle example

--- clases/16.React/02.gif-search/src/App.js
+++ clases/16.React/02.gif-search/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends React.Component {
+  componentWillMount(){ //Metodo UnSafe. Actualmente ya no se usa.
+    console.log('1. Antes de que se ejecute el render');
+  };
+
+  componentDidMount(){
+    console.log('3. Después de Render');
+  };
+
+  render() {
+    console.log('2. Se ejecuta al realizar el render');
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
+};
 
 export default App;

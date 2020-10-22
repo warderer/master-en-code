7104730eb46372564b98c8822ae7698986170931
César Guerra <warderer@gmail.com>
2020-10-21 20:58:05 -0500

    gif-search/logic implementation app, imagecard, searchbar

--- clases/16.React/02.gif-search/src/App.js
+++ clases/16.React/02.gif-search/src/App.js
@@ -2,8 +2,25 @@ import React from "react";
 import "./App.css";
 import SearchBar from './components/SearchBar';
 import ImageCard from './components/ImageCard';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  state = {
+    results: []
+  }
+
+  sendSearch = (search) => {
+    const apiKey = 'GeBHgHB4SbCMBiRBOWxdyQ9kdUpIzFoP';
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}&limit=10&offset=0&rating=g&lang=es`)
+    .then((response) => {
+        console.log(response.data.data);
+        this.setState({results: response.data.data});
+    }).catch((err) => {
+        console.log(err);
+    });
+  }
+
   componentWillMount(){ //Metodo UnSafe. Actualmente ya no se usa.
     console.log('1. Antes de que se ejecute el render');
   };
@@ -16,18 +33,11 @@ class App extends React.Component {
     console.log('2. Se ejecuta al realizar el render');
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar emitSearch={this.sendSearch} />
         <div className="grid-cards">
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+          {
+            this.state.results.map ( gif => (<ImageCard url={gif.images.fixed_width.url} />))
+          }
         </div>
       </div>
     );
--- clases/16.React/02.gif-search/src/components/SearchBar.js
+++ clases/16.React/02.gif-search/src/components/SearchBar.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
+    state = {
+        search: "",
+    }
+
+    // this.state --> Para manejar el estado INTERNO del componente.
+    // this.props --> Para manejar las propiedades qeu nos pasa el componente padre.
+
     render(){
         return(
             <div className="search-bar">
@@ -9,8 +16,18 @@ class SearchBar extends React.Component{
                     placeholder="¿Qué buscas?"
                     name="search"
                     className="search-input"
+                    onChange={(event)=> {
+                        // Se ejecuta cada vez que el usuario modifica algo en el input
+                        // onChange recibe un callback con el evento y así saber el valor
+                        this.setState({search: event.target.value})
+                    }}
                 />
-                <button className="search-button">Buscar </button>
+                <button
+                    className="search-button"
+                    /* Se usa una función anonomima para que tenga el control de ejecución y esta solo ocurra en el evento de clic*/
+                    onClick={() => this.props.emitSearch(this.state.search)}
+                >Buscar
+                </button>
             </div>
         )
     }

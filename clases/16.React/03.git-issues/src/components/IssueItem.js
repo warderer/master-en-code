100e07b09d60af86848f092f0ef595fc5db0f8eb
César Guerra <warderer@gmail.com>
2020-10-26 19:28:26 -0600

    git-issues/labels and bgcolors on issueItem

--- clases/16.React/03.git-issues/src/components/IssueItem.js
+++ clases/16.React/03.git-issues/src/components/IssueItem.js
@@ -6,6 +6,7 @@ class IssueItem extends React.Component{
         return(
             <div className="issues-container" key={this.props.url.id}>
                 <a href={this.props.url.html_url}>{this.props.url.title}</a>
+                {this.props.url.labels.map ( label => (<span className="label" style={{backgroundColor: '#'+label.color}}>{label.name}</span>))}
             </div>
         )
     }

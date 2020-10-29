83cff3a00268e2b5abc2d7f7526b5727a3e03c4b
César Guerra <warderer@gmail.com>
2020-10-28 19:34:20 -0600

    function-components/fix id - use api id

--- clases/16.React/04.function-components/src/components/HomeAPI.js
+++ clases/16.React/04.function-components/src/components/HomeAPI.js
@@ -17,8 +17,8 @@ function HomeAPI(){
     const getApiData = () => {
         axios.get(apiUri)
         .then((result) => {
-            const contactos = result.data.map((entry, index)=>{
-                return {id: index, name: entry.name, lastName: entry.last_name}
+            const contactos = result.data.map((entry)=>{
+                return {id: entry.id, name: entry.name, lastName: entry.last_name}
             })
             setContactos(contactos);
             console.log(contactos);

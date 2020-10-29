8708c424ea6c0554e335320903c3a01960799b0a
César Guerra <warderer@gmail.com>
2020-10-28 20:53:41 -0600

    function-components/react-router-dom example

--- clases/16.React/04.function-components/src/App.js
+++ clases/16.React/04.function-components/src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import HomeAPI from './components/HomeAPI'
+import Home from './components/Home'
+import About from './components/About'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HomeAPI/>
-      </header>
-    </div>
+    // Si voy  usar react-router-dom el primer componente siempre es <Router />
+    // Va a permitir detectar las URLs
+    <Router>
+      {/* Switch me va a permitir identificar la URL y pintar el componente adecuado */}
+      <Switch>
+        {/* Adentro de <Switch /> siempre va <Route /> */}
+        {/* Cuando entren al path "/" debe pintar el componente Home*/}
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Switch>
+
+    </Router>
   );
 }
 

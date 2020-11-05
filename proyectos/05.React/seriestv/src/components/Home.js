5adae3537d923befc06a9c41a96287be1e17e973
César Guerra <warderer@gmail.com>
2020-11-04 21:58:46 -0600

    seriestv/Api Request refactor and SingleShow basic api requests and display

--- proyectos/05.React/seriestv/src/components/Home.js
+++ proyectos/05.React/seriestv/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import ApiRequest from '../utils/ApiRequest'
 import ShowsList from './ShowsList';
 import Header from './Header';
 
@@ -7,31 +8,22 @@ function Home () {
     const UriSearch ='http://api.tvmaze.com/search/shows?q=';
     const [showList, setShowList] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
-
-    const getApiData = (query) => {
-        axios.get(UriSearch+query)
-        .then((result)=>{
-            //console.log(result);
-            setShowList(result.data);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+    
+    const updateFromSearch = (query) => {
+        ApiRequest(UriSearch,query,setShowList);
     }
 
     useEffect(()=>{
         console.log("Ejecución de Use Effect");
         setSearchQuery('Robot');
-        getApiData(searchQuery);
-        //console.log(showList);
-    },[]);
+        ApiRequest(UriSearch,searchQuery,setShowList);
+    },[searchQuery]);
 
     return(
         <div>
-            <Header updateData={getApiData}/>
+            <Header updateData={updateFromSearch}/>
             <h1>Shows</h1>
             {showList.length === 0 ? <h2>Cargando Shows</h2> : <ShowsList shows={showList} />}
-            
         </div>
         )
     

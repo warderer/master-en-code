24593121dc1a223596b7aec66514d5b111ea9a2f
César Guerra <warderer@gmail.com>
2020-11-05 19:01:28 -0600

    seriestv/Show general info component implementation

--- proyectos/05.React/seriestv/src/components/SingleShowView.js
+++ proyectos/05.React/seriestv/src/components/SingleShowView.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
-import ApiRequest from '../utils/ApiRequest'
+import ApiRequest from '../utils/ApiRequest';
+import ShowGeneralInfo from './showDetail/ShowGeneralInfo';
 
 function SingleShowView() {
     const params = useParams();
@@ -19,7 +20,7 @@ function SingleShowView() {
     return (
         <div>
             <h1> {`This is a View for the Show ID: ${params.idShow}`}</h1>
-            {Object.entries(singleShow).length === 0 ? <h2>Cargando información...</h2> : <h2>{singleShow.name}</h2> }
+            {Object.entries(singleShow).length === 0 ? <h2>Cargando información...</h2> : <h2><ShowGeneralInfo showData={singleShow}/></h2> }
             {Object.entries(episodes).length === 0 ? <h4>Cargando información...</h4> : episodes.map((episode,index)=><h4 key={index}>{episode.name}</h4>) }
             {Object.entries(cast).length === 0 ? <p>Cargando información...</p> : cast.map((actor,index)=> <p key={index}>{actor.person.name}</p>) }
         </div>
--- proyectos/05.React/seriestv/src/components/showDetail/ShowGeneralInfo.js
+++ proyectos/05.React/seriestv/src/components/showDetail/ShowGeneralInfo.js
@@ -1,9 +1,20 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-function ShowGeneralInfo() {
+function ShowGeneralInfo({showData}) {
     return (
         <div>
-            
+            <h2>{showData.name}</h2>
+            <img src={showData.image.medium}></img>
+            <p dangerouslySetInnerHTML={{
+                __html: showData.summary
+            }}/>
+            <p>Genres: {showData.genres.map((genre, index)=><span key={index}>{genre} </span>)}</p>
+            <span>Runtime: {showData.runtime} </span>
+            <span>Premiered: {showData.premiered} </span>
+            <span>Rating: {showData.rating.average} </span>
+            <span>Status: {showData.status} </span>
+            <span>Language: {showData.language}</span>
         </div>
     )
 }

98dadbef7ab08e26a55ed9370883f3c83b63de23
César Guerra <warderer@gmail.com>
2020-11-05 18:59:36 -0600

    seriestv/Small fix: series list with link

--- proyectos/05.React/seriestv/src/components/ShowsList.js
+++ proyectos/05.React/seriestv/src/components/ShowsList.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function showsList({shows}){
     const renderShows = () => {
-        return shows.map((element) => <h4 key={element.show.id}>{element.show.name}</h4>)
-    }
-
+        return shows.map((element) => 
+            <h4 key={element.show.id}><a href={'/show/'+element.show.id}>{element.show.name}</a></h4>
+        )}
     return (
         <div>
             {renderShows()}

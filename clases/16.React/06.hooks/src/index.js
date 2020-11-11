ca2cf415fe1bb6a539196b1cc9d973b8e37fc291
César Guerra <warderer@gmail.com>
2020-11-10 20:37:16 -0600

    hooks/useEffect and useState functionality with counter

--- clases/16.React/06.hooks/src/index.js
+++ clases/16.React/06.hooks/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+//import App from './App';
+import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );

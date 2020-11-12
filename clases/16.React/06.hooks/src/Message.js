144763f4784194848bc63defca0d2303ffb60679
César Guerra <warderer@gmail.com>
2020-11-11 21:29:12 -0600

    hooks/useRef correct use to keep data state

--- clases/16.React/06.hooks/src/Message.js
+++ clases/16.React/06.hooks/src/Message.js
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function MessageInput() {
 
     const [message, setMessage] = useState("");
-    const [counter, setCounter] = useState(0);
+    //const [counter, setCounter] = useState(0);
+    const counter = useRef(0);
 
     const sendMessage = () => {
         console.log("Mensaje enviado: ", message);
-        setCounter(counter + 1);
-        if(counter >= 5){
+        //setCounter(counter + 1);
+        if(counter.current >= 5){
             alert('Ya te pasaste de mensajes');
         }
+        counter.current += 1;
     }
 
     return(

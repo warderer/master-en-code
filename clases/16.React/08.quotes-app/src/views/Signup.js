db38f313add68b42ec9ab7b17550d112b68e29ac
César Guerra <warderer@gmail.com>
2020-11-18 20:07:22 -0600

    quotes-app/Signup hook object creation

--- clases/16.React/08.quotes-app/src/views/Signup.js
+++ clases/16.React/08.quotes-app/src/views/Signup.js
@@ -1,49 +1,92 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import useForm from '../hooks/useForm';
 
 function Signup () {
+
+    const sendData = (data) => {
+        console.log(data)
+    }
+
+    const { inputs, handleInputChange, handleSubmit } = useForm(sendData,{})
 
     return(
         <div>
             <Navbar />
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-5">
-                        <div className="form-group">
-                            <label htmlFor="">Nombre</label>
-                            <input type="text" className="form-control" name="first_name" id="first_name" />
+            <form onSubmit={handleSubmit}>
+                <div className="container mt-5">
+                    <div className="row justify-content-center">
+                        <div className="col-md-5">
+                            <div className="form-group">
+                                <label htmlFor="">Nombre</label>
+                                <input type="text"
+                                    value={inputs.first_name}
+                                    onChange={handleInputChange}
+                                    className="form-control"
+                                    name="first_name"
+                                    id="first_name"
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-md-5">
-                        <div className="form-group">
-                            <label htmlFor="">Apellidos</label>
-                            <input type="text" className="form-control" name="last_name" id="last_name" />
+                        <div className="col-md-5">
+                            <div className="form-group">
+                                <label htmlFor="">Apellidos</label>
+                                <input type="text" 
+                                    value={inputs.last_name}
+                                    onChange={handleInputChange}
+                                    className="form-control" 
+                                    name="last_name" 
+                                    id="last_name" 
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-md-5">
-                        <div className="form-group">
-                            <label htmlFor="">Email</label>
-                            <input type="email" className="form-control" name="email" id="email" />
+                        <div className="col-md-5">
+                            <div className="form-group">
+                                <label htmlFor="">Email</label>
+                                    <input type="email" 
+                                        value={inputs.email}
+                                        onChange={handleInputChange}
+                                        className="form-control" 
+                                        name="email" 
+                                        id="email" 
+                                    />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-md-5">
-                        <div className="form-group">
-                            <label htmlFor="">Password</label>
-                            <input type="password" className="form-control" name="password" id="password" />
+                        <div className="col-md-5">
+                            <div className="form-group">
+                                <label htmlFor="">Password</label>
+                                <input type="password"
+                                    value={inputs.password}
+                                    onChange={handleInputChange}
+                                    className="form-control"
+                                    name="password"
+                                    id="password" 
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-md-5">
-                        <div className="form-group">
-                            <label htmlFor="">Confirmar password</label>
-                            <input type="password" className="form-control" name="password_confirm" id="password_confirm" />
+                        <div className="col-md-5">
+                            <div className="form-group">
+                                <label htmlFor="">Confirmar password</label>
+                                <input type="password" 
+                                    value={inputs.password_confirm}
+                                    onChange={handleInputChange}
+                                    className="form-control" 
+                                    name="password_confirm" 
+                                    id="password_confirm" 
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-md-12 text-center">
+                            <button type="submit" className="btn btn-dark">Enviar</button>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
     

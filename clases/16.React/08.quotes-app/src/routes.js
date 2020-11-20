0b79b1e0f8f8e79aa27e8aeee6c1617ec98e839a
César Guerra <warderer@gmail.com>
2020-11-19 20:40:39 -0600

    quotes-app/Quotes view and route

--- clases/16.React/08.quotes-app/src/routes.js
+++ clases/16.React/08.quotes-app/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home';
 import Signup from './views/Signup';
 import Login from './views/Login';
+import Quote from './views/Quote';
 
 function Routes(){
     return(
@@ -11,6 +12,7 @@ function Routes(){
                 <Route path="/" exact component={Home}></Route>
                 <Route path="/signup" exact component={Signup}></Route>
                 <Route path="/login" exact component={Login}></Route>
+                <Route path="/quote" exact component={Quote}></Route>
             </Switch>
         </Router>
     )

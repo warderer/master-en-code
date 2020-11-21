5c7fa665814cce4d931fc2665d31eb46ac9f641c
César Guerra <warderer@gmail.com>
2020-11-20 22:11:34 -0600

    quotes-app/Logout feature

--- clases/16.React/08.quotes-app/src/routes.js
+++ clases/16.React/08.quotes-app/src/routes.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './views/Home';
 import Signup from './views/Signup';
 import Login from './views/Login';
 import Quote from './views/Quote';
 
+//Devuelve un JSX para que sea un componente de React
+const Logout = () => {
+    window.localStorage.removeItem('token');
+    return <Redirect to="/" />
+}
+
 function Routes(){
     return(
         <Router>
             <Switch>
-                <Route path="/" exact component={Home}></Route>
-                <Route path="/signup" exact component={Signup}></Route>
-                <Route path="/login" exact component={Login}></Route>
-                <Route path="/quote" exact component={Quote}></Route>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/signup" component={Signup}></Route>
+                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/quote" component={Quote}></Route>
+                <Route exact path="/logout" component={Logout}></Route>
             </Switch>
         </Router>
     )

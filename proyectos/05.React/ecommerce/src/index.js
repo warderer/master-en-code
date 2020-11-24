147e1f9e657104e13f3fee1f4b67822c18f20d34
César Guerra <warderer@gmail.com>
2020-11-23 21:56:18 -0600

    ecommerce/Basic routing and ignore .eslintcache

--- proyectos/05.React/ecommerce/src/index.js
+++ proyectos/05.React/ecommerce/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/scss/bootstrap.scss'; 
-import Home from './scenes/Home';
+import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );

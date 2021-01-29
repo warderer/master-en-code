a33da1c36cea7aa466e9b10dcc4d87544ff37a1b
César Guerra <warderer@gmail.com>
2021-01-28 20:03:05 -0600

    dummy-app/getServerSideProps users example and getStaticProps on Post example comparison

--- clases/17.Dummyapp/pages/posts/index.js
+++ clases/17.Dummyapp/pages/posts/index.js
@@ -2,13 +2,27 @@ import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import axios from 'axios';
 
+// const wait = () => new Promise((resolve) => {
+//     setTimeout(resolve,3000)
+// })
+
 // getStaticProps() es Propio de Next JS
 // Hace la llamada a API desde el lado del servidor, procesa el resultado
 // y los pasa como PROPS al componente para que entregue al cliente el HTML
 // con la información.
+
 export async function getStaticProps() {
     //Aquí puedo hacer llamadas a API y pasar el resultado como Props
     // Async/Await es otra forma de resolver promesas
+
+    // Cuando: BuildTime ---> Cuando hace build el proyecto ---
+    // Cuando corro npm run build o se vuelve a cargar
+    // el proyecto (hago un cambio en el código)
+    // Esto sucede realmente pocas veces, por lo que getStaticProps lo ocupo solo
+    // si los datos no ocupan cambiar constantemente
+
+    //await wait();
+
     const response = await axios.get('https://dummyapi.io/data/api/post?limit=10',{
         headers:{
             'app-id': '60121e4aa7384d693701c4b1'
--- clases/17.Dummyapp/pages/users/index.js
+++ clases/17.Dummyapp/pages/users/index.js
@@ -1,13 +1,36 @@
 import Head from 'next/head';
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Home.module.css';
+import axios from 'axios';
 
-export default function Users() {
+
+// getServerSideProps() es similar a getStaticProps()
+// Cuando: Se ejecuta cada vez que se hace una petición al servidor o cada vez que pido la ruta
+// Que: Cuando sabemos que los datos cambian muy seguido
+// Caso Especifico: CMS ---> Content Management System
+//
+export async function getServerSideProps() {
+    const response = await axios.get('https://dummyapi.io/data/api/user?limit=10',{
+        headers:{
+            'app-id': '60121e4aa7384d693701c4b1'
+    }}).catch(err => console.log(err));
+
+    const { data } = response;
+
+    return {
+        props:{
+            users:data
+        }
+    }
+};
+
+export default function Users(props) {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Users</title>
             </Head>
             <h2>En Users</h2>
+    {props.users.data.map(user => <li>{user.firstName}{' '}{user.lastName}</li>)}
         </div>
     )
 }

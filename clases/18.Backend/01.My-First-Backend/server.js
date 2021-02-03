d64c73dedc5f7d7ea6c90d88f4556b48c56b9be4
César Guerra <warderer@gmail.com>
2021-02-02 22:29:59 -0600

    mi-primer-backend/4 exercises, responses in server.js

--- clases/18.Backend/01.My-First-Backend/server.js
+++ clases/18.Backend/01.My-First-Backend/server.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 // Este archivo va a tener las configuraciones de mi server.
 
 // Llama la biblioteca de express (Import)
@@ -71,6 +72,63 @@ app.get('/api/pets', (req, res) => {
     res.send(petsArray);
 });
 
+/* EJERCICIOS: RESPUESTAS */
+// 1. - Agrega un endpoint '/api/' que responda a
+// una petición de tipo GET con un código de estado 200
+// y el siguiente json:
+// { 'mensaje': 'hola mundo' }
+app.get('/api/',(req,res)=>{
+    res.send({mensaje:'hola mundo'});
+});
+
+// 2. - Agrega un endpoint '/api/suma' que responda a una
+// petición de tipo GET con la suma de dos números que
+// reciba mediante las querys num1 y num2.El servidor
+// debe responder con un código de estado 200 y un json
+// como el siguiente:
+// { 'resultado': 7 }
+app.get('/api/suma',(req,res)=>{
+    if (req.query.num1&&req.query.num2) {
+        suma = parseInt(req.query.num1) + parseInt(req.query.num2);
+        res.send({resultado:suma});
+    } else {
+        res.status(400).send('Error: No valid values')
+    }
+});
+
+// 3. - Agrega un endpoint '/api/usuario/' que responda a
+// una petición de tipo GET con el nombre que sea
+// recibido a través de params.El servidor debe responder
+// con un código de estado 200 y un json como este:
+// { 'usuario': 'Edwin' }
+app.get('/api/usuario/:nombre',(req,res)=>{
+    const {nombre} = req.params;
+    res.status(200).send({usuario:nombre});
+})
+
+// 4. - Agrega un endpoint '/api/swapi' que responda a una
+// petición de tipo GET con el personaje solicitado de
+// https://swapi.dev/
+// El cliente debe mandar el número de personaje mediante
+// params.La respuesta del servidor debe lucir algo así
+// {
+//     'personaje': {
+//         'name': 'Luke Skywalker',
+//                         ...,
+//     }
+// }
+app.get('/api/swapi/:idPersonaje', async (req,res)=>{
+    const id = req.params.idPersonaje;
+    try {
+        const swapiRes = await axios.get(`https://swapi.dev/api/people/${id}`);
+        res.send(swapiRes.data); //status 200
+    }
+    catch(err) {
+        res.status(err.response.status).json({
+            error: err.message
+        })
+    }
+});
 
 // Levanta el servidor en un puerto y recibe un Callback
 app.listen(3000, () => {

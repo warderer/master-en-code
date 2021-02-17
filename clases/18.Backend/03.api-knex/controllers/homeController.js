a6e62af42d2299559649ac15368555be64485af2
César Guerra <warderer@gmail.com>
2021-02-16 20:19:56 -0600

    api-knex/Home model and controller create implementation

--- clases/18.Backend/03.api-knex/controllers/homeController.js
+++ clases/18.Backend/03.api-knex/controllers/homeController.js
@@ -1,6 +1,16 @@
+const ModelHome = require('../models/Home');
+
 const createHome = (req, res) => {
     //Aqui es donde debo crear mi HOME
-    res.send({ message: 'Home creado (FAKE)' })
+    //Validar que todo este bien y ejecutar mi create del Modelo.
+    ModelHome.create(req.body)
+    .then((row) => {
+        res.status(201).send(row);
+    })
+    .catch((err)=> {
+        res.status(400).send(err);
+    })
+    //res.send({ message: 'Home creado (FAKE)' })
 };
 
 module.exports = {
---
+++ clases/18.Backend/03.api-knex/models/Home.js
@@ -0,0 +1,26 @@
+// El Modelo trae los datos de la base de datos.
+// No se encarga de validar datos ni resolver promesas, eso es trabajo del controlador en MVC
+
+// Conectarme a la base de datos
+// Config exporta la configuración de la INSTANCIA activa de la base de datos con sus detalles de conexión.
+const knex = require('../config');
+
+const create = (bodyHome) => {
+    // Crear registro en la tabla HOMES
+    // bodyHome es un objeto que contiene los valores a insertar
+
+    return knex // Retorno una promesa para que la logica se maneje (.then y .catch) a nivel del controlador, respetando MVC.
+        .insert(bodyHome) // ¿Qué datos voy a insertar? {title: titulo, address: 'x', guests: 5 }
+        .returning(['title','house_id','address','guests','description','created_at']) //¿Que voy a regresar? - Select house_id, title ...
+        .into('homes') // ¿De que tabla? - FROM homes
+    };
+
+const findAll = () => {
+    // Obtiene todos los registros de la tabla HOMES
+
+};
+
+module.exports = {
+    create,
+    findAll,
+};

1a014a6e21774ffb78633106ec763364ff0994b2
César Guerra <warderer@gmail.com>
2021-03-02 19:56:45 -0600

    api-knex/small typo fix

--- clases/18.Backend/03.api-knex/utils/generateJWT.js
+++ clases/18.Backend/03.api-knex/utils/generateJWT.js
@@ -2,9 +2,9 @@ const JWT = require('jsonwebtoken');
 //firma, esto debe ir como variable de entorno
 const SECRET_KEY = '6cYPrkv2HNzX5ehJyrww3A8t1gxYKzUtNu3EIhdfCMhGZCrZf+W53g=='
 
-module.exports = ({id_user, rol, email}) => {
+module.exports = ({user_id, rol, email}) => {
     const payload = {
-        id_user,
+        user_id,
         rol,
         email
     }

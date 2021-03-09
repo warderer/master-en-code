52add7a1b55647fd5bab6a79353655843aa9246e
César Guerra <warderer@gmail.com>
2021-03-08 21:25:20 -0600

    api-mongo/env + basic get and post with pure mongo

--- clases/18.Backend/04.api-mongo/server.js
+++ clases/18.Backend/04.api-mongo/server.js
@@ -1,24 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const MONGO_URI = "CADENA DE CONEXION MONGO";
+const { MONGO_URI } = process.env; //Variable de entorno con datos de conexión
 
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
 
-app.listen(3000,() => {
-    console.log('SERVER ON');
+app.get('/users', (req, res) => {
+    //crear un nuevo user
     MongoClient.connect(MONGO_URI, {
         useUnifiedTopology: true, //Evitar DeprecationWarning: current Server Discovery and Monitoring engine is deprecated
         useNewUrlParser: true,
     })
-        .then( (db) => {
-            console.log('Conected to Database');
+    .then( (db) => {
+        console.log('Conected to Database');
+        const database = db.db('apimongo');
+        const collection = database.collection('users');
+        collection.find({}).toArray(function(error, results){
+            res.status(200).send(results);
             db.close(); //Cerramos la conexión a la DB por seguridad.
         })
-        .catch( (err) => {
-            console.log(`DB Connection Error: ${err.message}`);
+    })
+    .catch( (err) => {
+        console.log(`DB Connection Error: ${err.message}`);
+    });
+});
+
+app.post('/users', (req, res) => {
+    //crear un nuevo user
+    MongoClient.connect(MONGO_URI, {
+        useUnifiedTopology: true, //Evitar DeprecationWarning: current Server Discovery and Monitoring engine is deprecated
+        useNewUrlParser: true,
+    })
+    .then( (db) => {
+        console.log('Conected to Database');
+        const database = db.db('apimongo');
+        const collection = database.collection('users');
+        collection.insertOne(req.body, function(err,user){
+            if(err) throw err;
+            res.status(201).send(user);
+            db.close(); //Cerramos la conexión a la DB por seguridad.
         });
+    })
+    .catch( (err) => {
+        console.log(`DB Connection Error: ${err.message}`);
+    });
+});
+
+app.listen(3000,() => {
+    console.log('SERVER ON');
+
 });
 

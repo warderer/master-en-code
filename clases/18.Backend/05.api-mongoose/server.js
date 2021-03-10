17123a243d1bf7045ec29b249c83ec5f1617c152
César Guerra <warderer@gmail.com>
2021-03-09 22:48:03 -0600

    api-mongoose/basic users schema and route implementation

--- clases/18.Backend/05.api-mongoose/server.js
+++ clases/18.Backend/05.api-mongoose/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const Users = require('./Users');
 
 const app = express();
 const { MONGO_URI } = process.env; //Variable de entorno con datos de conexión
@@ -22,6 +23,20 @@ db.on('error', function(err){ // Se ejecuta cada vez que hay un error en la cone
 
 db.once('open', function(){ // Se ejecuta una vez y solo cuando se completo la conexión
     console.log('Conected to database!!');
+});
+
+app.get('/users', (req,res) => {
+    Users.find({}).then((result) => { //mongoose regresa una promesa y lo trae en arreglo
+        res.status(200).send(result)
+    });
+});
+
+app.post('/users', (req,res) => {
+    Users.create(req.body).then((user) => {
+        res.status(201).send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    })
 });
 
 app.listen(3000,() => {

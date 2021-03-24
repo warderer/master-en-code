4230cfa1cc0cd6f746db7df9e2c7a534c48ddc2c
César Guerra <warderer@gmail.com>
2021-03-23 21:55:29 -0600

    api-moongose/fix image requirement

--- clases/18.Backend/05.api-mongoose/middlewares/manageFiles.js
+++ clases/18.Backend/05.api-mongoose/middlewares/manageFiles.js
@@ -2,11 +2,12 @@ const uploadImage = require('../utils/storage');
 module.exports = (req, res, next) => {
 
     if(process.env.NODE_ENV === "production") {
-        if(!req.file) res.status(400).send({message:"No se envio ninguna imagen"});
+        if(!req.file) return next(); //No hago obligatorio subir una imagen
         const url = uploadImage(req.file);
         req.body.photo = url;
         console.log("Production")
     } else {
+        if(!req.file) return next(); //No hago obligatorio subir una imagen
         req.body.photo = `${req.protocol}://${req.host}/${req.file.path}` // en path viene la ubicación de mi archivo.
         console.log("No es production")
     }

b380f8a29188502616fdef99098c4292228ccb7c
César Guerra <warderer@gmail.com>
2021-04-20 20:26:13 -0500

    carga-datos-csv/Data preparation process for mongo

--- clases/19.Trends/01.carga-datos-csv/loadDataFromCsv.js
+++ clases/19.Trends/01.carga-datos-csv/loadDataFromCsv.js
@@ -7,15 +7,31 @@
 // ----------------------------------
 
 
-// 2.- Abrir el archivo.
+// 1.- Abrir el archivo.
 const csv = require('csv-parser');
 const fs = require('fs');
 
 const results = [];
 
-fs.createReadStream('v.csv') // Lee un archivo y lo convierte en una linea de datos que se pueda leer
+fs.createReadStream('e.csv') // Lee un archivo y lo convierte en una linea de datos que se pueda leer
     .pipe(csv()) // Pipe transforma datos de un stream a Algo. (Ese algo yo lo puedo crear)
-    .on('data', (data) => results.push(data)) // Escuchadores de los eventos que detona el pipe y que su vez detona csv(), data = row del csv
+    // 2.- Leer cada row del archivo
+    .on('data', (data) => {
+        // 3.- Procesar / Limpiar los datos.
+        // Crear el objeto para mongo
+        const sessionClient = {
+            device_mac: data.device_mac,
+            branch_office: parseInt(data.branch_office),
+            month_tz: parseInt(data.month_tz),
+            day_tz: parseInt(data.day_tz),
+            day_of_week_tz: data.day_of_week_tz,
+            hour_tz: parseInt(data.hour_tz),
+            connection_date: new Date(`2016-${data.month_tz}-${data.day_tz}T${data.hour_tz}:00:00Z`),
+            visitor: data.visitor === 'true',
+            tiempodeses: data.tiempodeses
+        };
+        results.push(sessionClient)
+    }) // Escuchadores de los eventos que detona el pipe y que su vez detona csv(), data = row del csv
     .on('end', () => {
         console.log(results);
-    });
+    })

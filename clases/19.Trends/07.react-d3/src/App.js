619984365352a4b9ce7e806e2d2c2e53f8fbab22
César Guerra <warderer@gmail.com>
2021-05-04 20:47:23 -0500

    react-d3/BarChart with virtualDom approach using d3

--- clases/19.Trends/07.react-d3/src/App.js
+++ clases/19.Trends/07.react-d3/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Bar from './Bar';
+import BarChart from './BarChart';
 
 function App() {
   return (
     <div className="App">
       <h1>React y D3</h1>
-      <Bar />
+      {/* <Bar /> */}
+      <BarChart />
     </div>
   );
 }

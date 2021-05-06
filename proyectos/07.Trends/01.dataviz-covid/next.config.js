705eb46dcc89ee05508dae83bcf1b6dc5df0fd6b
César Guerra <warderer@gmail.com>
2021-05-06 12:37:02 -0500

    dataviz-covid/project init

---
+++ proyectos/07.Trends/01.dataviz-covid/next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+        test: /\.csv$/,
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true
+        }
+      })
+
+    return config
+  }
+}
